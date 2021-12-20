8db508542443a793c24942fbbdc3a089b1c9b1ac
Sarah Ni <sarah.ni@helloholo.sg>
2021-12-20 11:22:09 +0000

    Update collection_stats.py

--- collection_stats.py
+++ collection_stats.py
@@ -22,8 +22,7 @@ def store_rec20_sales(collection):
          name['event_timestamp'] = text['assets'][i]['last_sale']['event_timestamp']
          name['eth_price'] = text['assets'][i]['last_sale']['payment_token']['eth_price']
          name['usd_price'] = text['assets'][i]['last_sale']['payment_token']['usd_price']
-         number = text['assets'][i]['id']
-         collections[number] = name
+         collections['values'] = name
       return collections
 
    except:
