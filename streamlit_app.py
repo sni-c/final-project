d4f8c3221c6c791c0092f4bf052ee3f4a1c38c64
Sarah Ni <sarah.ni@helloholo.sg>
2021-12-22 23:10:47 +0000

    Barebone streamlit app raw data

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,49 @@
 # streamlit_app.py
-#!/usr/bin/env python
+
 import streamlit as st
-import snowflake as sf
-from snowflake import connector
+import snowflake.connector
+import pandas as pd
+from PIL import Image
 
-# Everything is accessible via the st.secrets dict:
+# Initialize connection.
+# Uses st.cache to only run once.
+@st.cache(allow_output_mutation=True, hash_funcs={"_thread.RLock": lambda _: None})
+def init_connection():
+    return snowflake.connector.connect(**st.secrets["snowflake"])
 
-st.write("DB username:", st.secrets["db_username"])
-st.write("DB password:", st.secrets["db_password"])
-st.write("My cool secrets:", st.secrets["my_cool_secrets"]["things_i_like"])
+conn = init_connection()
 
-# And the root-level secrets are also accessible as environment variables:
+# Perform query.
+# Uses st.cache to only rerun when the query changes or after 10 min.
+@st.cache(ttl=600)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
 
-import os
-st.write(
-	"Has environment variables been set:",
-	os.environ["db_username"] == st.secrets["db_username"])
+collection_stats = run_query("SELECT * from STG_COLLECTION ORDER BY CREATE_TIME DESC LIMIT 10;")
+
+st.title('Latest NFT Trends')
+st.markdown('NFT Collections by Trading Volume:')
+coll_df = pd.DataFrame(collection_stats)
+st.table(coll_df)
+
+collection_name = run_query("SELECT COLLECTIONSLUG from STG_COLLECTION ORDER BY CREATE_TIME DESC LIMIT 10;")
+query1 = "SELECT * from STG_SALES WHERE COLLECTIONSLUG = '%s' ORDER BY CREATE_TIME DESC LIMIT 10;"
+queryimg = "SELECT IMAGE_URL from STG_SALES WHERE COLLECTIONSLUG = '%s' QUALIFY ROW_NUMBER() OVER (PARTITION BY IMAGE_URL ORDER BY IMAGE_URL DESC) = 1;"
+
+st.title('Recent Sales Per Collection')
+st.markdown('10 recent sales per Top 10 collection:')
+
+for i in range(0,10):
+    query2 = query1 % collection_name[i][0]
+    sales_stats = run_query(query2) 
+    stats_df = pd.DataFrame(sales_stats)
+    st.table(stats_df)
+
+    st.markdown('NFT Viz')
+    queryimg2 = queryimg % collection_name[i][0]
+    sales_images = run_query(queryimg2)
+    st.image(sales_images[i][0], width = 200)
+
+
