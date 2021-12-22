7fd657f7e5a454e4f70d9c6e5f69b70b2cc88f11
Sarah Ni <sarah.ni@helloholo.sg>
2021-12-22 21:07:21 +0000

    Add streamlit app file 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,27 @@
+# streamlit_app.py
+
+import streamlit as st
+import snowflake.connector
+
+# Initialize connection.
+# Uses st.cache to only run once.
+@st.cache(allow_output_mutation=True, hash_funcs={"_thread.RLock": lambda _: None})
+def init_connection():
+    return snowflake.connector.connect(**st.secrets["snowflake"])
+
+conn = init_connection()
+
+# Perform query.
+# Uses st.cache to only rerun when the query changes or after 10 min.
+@st.cache(ttl=600)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
+
+rows = run_query("SELECT * from STG_COLLECTION;")
+
+# Print results.
+for row in rows:
+    st.write(f"{row[0]} has a :{row[1]}:")
 # streamlit app
