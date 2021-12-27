a7f904dd018a2938ce43ede0a6c77c57d696f5b5
Sarah Ni <sarah.ni@helloholo.sg>
2021-12-27 18:19:43 +0000

    Update python venv

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ from plotly.colors import n_colors
 
 # Initialize connection.
 # Uses st.cache to only run once.
-@st.cache(allow_output_mutation=True, hash_funcs={"_thread.RLock": lambda _: None})
+# @st.cache(allow_output_mutation=True, hash_funcs={"_thread.RLock": lambda _: None})
 def init_connection():
     return snowflake.connector.connect(**st.secrets["snowflake"])
 
